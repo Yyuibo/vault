134e1dd525117d6eda4ef6a1a3be3af61f4ad654
tzl <ubuntu@tzl>
2023-04-21 14:48:58 +0000

    update ExpandPoly

--- src/test/test_server.cc
+++ src/test/test_server.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstddef>
+#include <map>
 #include <emp-sh2pc/emp-sh2pc.h>
 #include "encode/encode.h"
 // #include "key_binding_and_retrieval/cs_functions.h"
@@ -20,6 +21,34 @@ void SendData(emp::NetIO * io, uint64_t data) {
     io->send_data(bytes, 8);
 }
 
+
+void ExpandPoly(std::vector<uint8_t> P, std::vector<uint64_t> &res, uint64_t module) {
+    int n = P.size();
+    res[0] = (uint64_t)P[0];
+    res[1] = 1;
+    for (int i = 1; i < n; i++) {
+        std::vector<uint64_t> m1(n + 1, 0);
+        std::vector<uint64_t> m2(n + 1, 0);
+
+        for(int j = 0; j <= n; j++) {
+            if(res[j] != 0)
+                m1[j + 1] += res[j];
+        }
+        for(int j = 0; j <= n; j++) {
+            if(res[j] != 0)
+                m2[j] += res[j] * (uint64_t)P[i];
+        }
+        for(int j = 0; j <= n; j++) {
+            res[j] = (m1[j] + m2[j]) % module;
+        }
+    }
+    // (x - a)(x - b) = x^2 -(a + b)x + ab
+    for (int i = n - 1; i >= 0; i = i - 2) {
+        res[i] = (module - res[i]) % module;
+        // res[i + 1] = res[i] % module;
+    }
+}
+
 uint64_t res_cmn = 0;
 void Cmn(int depth, std::vector<uint64_t> res, std::vector<uint64_t> P) {
     if (depth == 1) {
@@ -27,11 +56,11 @@ void Cmn(int depth, std::vector<uint64_t> res, std::vector<uint64_t> P) {
         for (int i = 0; i < res.size(); i++) {
             res_cmn += res[i];
             res_cmn %= 8519681;
-        }
+        } 
         return;
     }
     
-    for (int i = 0; i < res.size(); i++) {
+    for (int i = 0; i < res.size() - 1; i++) {
         std::vector<uint64_t> tmp(P.size() - 1);
         for (int j = 1; j < P.size(); j++) {
             tmp[j - 1] = (res[i] * P[j]) % 8519681;
@@ -45,47 +74,20 @@ int main(int argc, char** argv) {
     std::string file = "../src/encode/embedding_vector_server.txt";
     std::vector<uint8_t> P;
     GetEncodeRes(file, P);
-    // for test
-    P.resize(20);
-    
+ 
     // the secret polynomial κ of degree smaller than k
     std::vector<uint32_t> x = {0, 1};
     std::vector<uint32_t> y = {0x0001, 0x03};
     std::vector<uint64_t> coeff_k;
     polynomial_from_points(x, y, coeff_k, 8519681);
 
-    // V(X)
+    // V(X) of degree = P.size()
     std::vector<uint64_t> coeff_V(P.size() + 1, 0);
-    cout << "polynomial expansion" << endl;
-    std::vector<uint64_t> res;
-    std::vector<uint64_t> P1;
-    res.resize(P.size());
-    P1.resize(P.size());
-
-    // polynomial expansion
-    for (int i = 0; i < coeff_V.size() - 1; i++) {
-        res_cmn = 0;
-        for (int i = 0; i < res.size(); i++) {
-            res[i] = (uint64_t)P[i];
-            P1[i] = (uint64_t)P[i];
-        }
-        Cmn(i + 1, res, P1);
-        if (i % 2 == 0) {
-            coeff_V[coeff_V.size() - i - 2] = 8519681 - res_cmn;
-        }
-        else {
-            coeff_V[coeff_V.size() - i - 2] = res_cmn;
-        }
-    }
+    ExpandPoly(P, coeff_V, 8519681);
     for (int i = 0; i < coeff_k.size(); i++) {
         coeff_V[i] += coeff_k[i];
     }
-    coeff_V[coeff_V.size() - 1] = 1;
-    // for (int i = 0; i < coeff_V.size(); i++) {
-    //     cout << coeff_V[i] << endl;
-    // }
-    // cout << (uint32_t)P[17] << endl;
-    // cout << calculate_poly(coeff_V, (uint32_t)P[17]) << endl;
+
 
     int port, party;
     emp::parse_party_and_port(argv, &party, &port);
